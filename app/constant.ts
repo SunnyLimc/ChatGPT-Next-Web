194e3e18a576b29ad8512f9d660c22b596de65ee
Limc <i@limc.me>
2023-06-18 11:46:19 +0800

    chore: CI and support URL changes

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-export const OWNER = "Yidadaa";
+export const OWNER = "SunnyLimc";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
@@ -6,7 +6,7 @@ export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
-export const DEFAULT_API_HOST = "https://chatgpt1.nextweb.fun/api/proxy";
+export const DEFAULT_API_HOST = "https://chatgptnextweb.uniq.moe/api/proxy";
 
 export enum Path {
   Home = "/",
